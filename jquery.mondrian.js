2449157af53679b059289d20e2dd2baa7598d574
Simon Scarfe <simon.scarfe@gmail.com>
2010-12-26 17:09:32 +0000

    got it initially working on body elements. Few hacks in there, but we can fix them in time

--- jquery.mondrian.js
+++ jquery.mondrian.js
@@ -8,16 +8,17 @@
         return this.each(function() {
             $this = $(this);
             var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
-			var width = $this.width(),
-				height = $this.height();
+			var width = $(window).width(), //$this.width(),
+				height = $(window).height(); //$this.height();
 
 			var canvas = $('<canvas id="cnv" width="'+width+'" height="'+height+'"/>')
 						     .width(width)
 							 .height(height)
 							 .css('background-color',o.background)
-							 .css({'position':'absolute',
+							 .css({'position':'fixed',
 								   'top':'0',
-								   'left':'0'});
+								   'left':'0',
+								   'z-index':'-1'});
 
 			var x_coords = [],
 				y_coords = [];
@@ -40,7 +41,27 @@
 			$.each(y_coords, function(i,v) {
 				$this.mondrian.lineY(ctx, v);
 			});
+			x_coords.push(0);
+			y_coords.push(0);
 
+			x_coords.sort(function(a,b) {
+				return a-b;
+			});
+			y_coords.sort(function(a,b) {
+				return a-b;
+			});
+
+			$.each(o.colors, function(i,v) {
+				var x = Math.floor(Math.random()*x_coords.length),
+					y = Math.floor(Math.random()*y_coords.length),
+					x1 = x_coords[x],
+					x2 = (x < x_coords.length-1) ? x_coords[x+1] : width,
+					rect_width = Math.abs(x2-x1),
+					y1 = y_coords[y],
+					y2 = (y < y_coords.length-1) ? y_coords[y+1] : height,
+					rect_height = Math.abs(y2-y1);
+				$this.mondrian.fillRect(ctx,y1,x1,rect_height,rect_width,v)
+			});
 			canvas.prependTo($this);
         });
     };
@@ -57,6 +78,13 @@
 		ctx.moveTo(x,0);
 		ctx.lineTo(x,ctx.canvas.height);
 		ctx.stroke();
+	}
+
+	$.fn.mondrian.fillRect = function(ctx, x1,y1, w,h, color) {
+		ctx.fillStyle = color;
+		ctx.fillRect(x1,y1,w,h);
+		ctx.strokeRect(x1,y1,w,h);
+		ctx.stroke()
 	}
 
     $.fn.mondrian.defaults = {
