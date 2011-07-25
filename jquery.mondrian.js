2172f8d41f0b1be97094fb46ddb144a1c87be361
Simon Scarfe <simon.scarfe@gmail.com>
2011-07-25 23:30:59 +0100

    refactored and recoded to repaint on window resize. Should provide a small band aid to issue #1

--- jquery.mondrian.js
+++ jquery.mondrian.js
@@ -7,65 +7,91 @@
 
         return this.each(function() {
             $this = $(this);
+
+
             var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
-			var width = $(window).width(), //$this.width(),
-				height = $(window).height(); //$this.height();
+			$this.mondrian.paint($this, o);
 
-			var canvas = $('<canvas id="cnv" width="'+width+'" height="'+height+'"/>')
-						     .width(width)
-							 .height(height)
-							 .css('background-color',o.background)
-							 .css({'position':'fixed',
-								   'top':'0',
-								   'left':'0',
-								   'z-index':'-1'});
+			$(window).resize(function() {
+				var canvas = $('#cnv');
+				canvas.remove();	
 
-			var x_coords = [],
-				y_coords = [];
-
-			for(i = 0; i < o.linesX; i++) {
-				x_coords.push(Math.floor(Math.random() * height));
-			}
-			for(i = 0; i < o.linesY; i++) {
-				y_coords.push(Math.floor(Math.random() * width));
-			}
-
-			var ctx = canvas[0].getContext("2d");
-			ctx.strokeStyle = o.foreground;
-			ctx.lineWidth = o.lineWidth;
-
-			$.each(x_coords, function(i,v) {
-				$this.mondrian.lineX(ctx, v);
+			$this.mondrian.paint($this, o);
 			});
-
-			$.each(y_coords, function(i,v) {
-				$this.mondrian.lineY(ctx, v);
-			});
-			x_coords.push(0);
-			y_coords.push(0);
-
-			x_coords.sort(function(a,b) {
-				return a-b;
-			});
-			y_coords.sort(function(a,b) {
-				return a-b;
-			});
-
-			$.each(o.colors, function(i,v) {
-				var x = Math.floor(Math.random()*x_coords.length),
-					y = Math.floor(Math.random()*y_coords.length),
-					x1 = x_coords[x],
-					x2 = (x < x_coords.length-1) ? x_coords[x+1] : width,
-					rect_width = Math.abs(x2-x1),
-					y1 = y_coords[y],
-					y2 = (y < y_coords.length-1) ? y_coords[y+1] : height,
-					rect_height = Math.abs(y2-y1);
-				$this.mondrian.fillRect(ctx,y1,x1,rect_height,rect_width,v)
-			});
-			canvas.prependTo($this);
         });
     };
     
+	$.fn.mondrian.paint = function($this, o) {
+		var width = $(window).width(), //$this.width(),
+			height = $(window).height(); //$this.height();
+
+		var canvas = $this.mondrian.createCanvas(width, height, o.background),
+			x_coords = $this.mondrian.generateCoordinates(o.linesX, height),
+			y_coords = $this.mondrian.generateCoordinates(o.linesY, width);
+
+		var ctx = canvas[0].getContext("2d");
+		ctx.strokeStyle = o.foreground;
+		ctx.lineWidth = o.lineWidth;
+
+		ctx = $this.mondrian.paintCanvas(ctx, x_coords, y_coords, width, height, o.colors);
+		canvas.prependTo($this);
+	}
+
+  	$.fn.mondrian.createCanvas = function(width, height, background) {
+		return $('<canvas id="cnv" width="'+width+'" height="'+height+'"/>')
+				.width(width)
+				.height(height)
+				.css('background-color',background)
+				.css({'position':'fixed',
+					'top':'0',
+					'left':'0',
+					'z-index':'-1'});
+	}
+	
+	$.fn.mondrian.generateCoordinates = function(max_lines, max_value) {
+		var coords = [];
+
+		for(var i = 0; i < max_lines; i++) {
+			coords.push(Math.floor(Math.random() * max_value));
+		}
+
+		return coords;
+	}
+
+	$.fn.mondrian.paintCanvas = function(ctx, x_coords, y_coords, width, height, colors) {
+		$.each(x_coords, function(i,v) {
+			$this.mondrian.lineX(ctx, v);
+		});
+
+		$.each(y_coords, function(i,v) {
+			$this.mondrian.lineY(ctx, v);
+		});
+		x_coords.push(0);
+		y_coords.push(0);
+
+		x_coords.sort(function(a,b) {
+			return a-b;
+		});
+		y_coords.sort(function(a,b) {
+			return a-b;
+		});
+
+		$.each(colors, function(i,v) {
+			var x = Math.floor(Math.random()*x_coords.length),
+				y = Math.floor(Math.random()*y_coords.length),
+				x1 = x_coords[x],
+				x2 = (x < x_coords.length-1) ? x_coords[x+1] : width,
+				rect_width = Math.abs(x2-x1),
+				y1 = y_coords[y],
+				y2 = (y < y_coords.length-1) ? y_coords[y+1] : height,
+				rect_height = Math.abs(y2-y1);
+			$this.mondrian.fillRect(ctx,y1,x1,rect_height,rect_width,v)
+		});
+
+		return ctx;
+
+	}
+
     $.fn.mondrian.lineX = function(ctx,y) {
 		ctx.beginPath();
 		ctx.moveTo(0,y);
